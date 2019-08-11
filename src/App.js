0531cc4dba1f6b7e1fd9bfd78b5fab5f31a1ac1e
Johnny Kingi / 10005351 <jknz84@gmail.com>
2019-08-12 11:57:57 +1200

    Basic React app started, app.js working as functional component opposed to class based component. Making use of useState() and destructuring to update state.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Person from './Person/Person'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+let name = 'Big PaPa';
+let age = 29;
 
-export default App;
+const app = props => {
+  //state property
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'J Dawg', age: 28},
+      { name: 'P Dawg', age: 69},
+      { name: 'JLawrence', age: 29}
+    ],
+    otherstate: 'some other value'
+  });
+
+  const eventHandler = () => {
+  //alert('Clicked');
+  // DONT CHANGE STATE LIKE THIS: this.state.persons[0].name = 'JK';
+  setPersonsState({
+      persons: [
+        {name: 'JK', age: 34},
+        { name: 'P Dawg', age: 69},
+        { name: 'JLawrence', age: 25}
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <p className="display-1">Hello World</p>
+      <input type="text"></input><br className="mt-2"></br>
+      <button type="submit" onClick={eventHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> child 1 of Person</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> child 2 of Person</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
+    </div>
+  );
+};
+
+
+export default app;
+/*
+state = {
+  persons: [
+    { name: 'J Dawg', age: 28},
+    { name: 'P Dawg', age: 69},
+    { name: 'JLawrence', age: 29}
+  ],
+  otherstate: 'some other value'
+};
+eventHandler = () => {
+  //alert('Clicked');
+  // DONT CHANGE STATE LIKE THIS: this.state.persons[0].name = 'JK';
+  this.setState({
+    persons: [
+      {name: 'JK', age: 34},
+      { name: 'P Dawg', age: 69},
+      { name: 'JLawrence', age: 25}
+    ]
+  });
+};
+*/
