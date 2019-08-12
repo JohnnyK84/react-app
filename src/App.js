2e87f52bedd920f536cf8afae920f03d9f8172af
Johnny Kingi / 10005351 <jknz84@gmail.com>
2019-08-12 13:04:09 +1200

    modified App.js back to Class based component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Person from './Person/Person'
@@ -6,21 +6,34 @@ import Person from './Person/Person'
 let name = 'Big PaPa';
 let age = 29;
 
-const app = props => {
+class App extends Component {
   //state property
-  const [ personsState, setPersonsState ] = useState({
-    persons: [
-      { name: 'J Dawg', age: 28},
-      { name: 'P Dawg', age: 69},
-      { name: 'JLawrence', age: 29}
-    ],
-    otherstate: 'some other value'
-  });
+    state = {
+      persons: [
+        { name: 'J Dawg', age: 28},
+        { name: 'P Dawg', age: 69},
+        { name: 'JLawrence', age: 29}
+      ],
+      otherstate: 'some other value'
+    };
 
-  const eventHandler = () => {
-  //alert('Clicked');
-  // DONT CHANGE STATE LIKE THIS: this.state.persons[0].name = 'JK';
-  setPersonsState({
+  render() {    
+    return (
+      <div className="App">
+        <p className="display-1">Hello World</p>
+        <input type="text"></input><br className="mt-2"></br>
+        <button type="submit" onClick={this.eventHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> yeah Ghee</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> yeah Ghee</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+    );
+  };
+
+  eventHandler = () => {
+    //alert('Clicked');
+    // DONT CHANGE STATE LIKE THIS: this.state.persons[0].name = 'JK';
+    this.setState({
       persons: [
         {name: 'JK', age: 34},
         { name: 'P Dawg', age: 69},
@@ -28,39 +41,6 @@ const app = props => {
       ]
     });
   };
-
-  return (
-    <div className="App">
-      <p className="display-1">Hello World</p>
-      <input type="text"></input><br className="mt-2"></br>
-      <button type="submit" onClick={eventHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> child 1 of Person</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> child 2 of Person</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
-    </div>
-  );
 };
 
-
-export default app;
-/*
-state = {
-  persons: [
-    { name: 'J Dawg', age: 28},
-    { name: 'P Dawg', age: 69},
-    { name: 'JLawrence', age: 29}
-  ],
-  otherstate: 'some other value'
-};
-eventHandler = () => {
-  //alert('Clicked');
-  // DONT CHANGE STATE LIKE THIS: this.state.persons[0].name = 'JK';
-  this.setState({
-    persons: [
-      {name: 'JK', age: 34},
-      { name: 'P Dawg', age: 69},
-      { name: 'JLawrence', age: 25}
-    ]
-  });
-};
-*/
+export default App;
