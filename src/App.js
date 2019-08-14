eb31487e46f8714fced1d2771bb24030f4b46f62
Johnny Kingi / 10005351 <jknz84@gmail.com>
2019-08-15 11:11:41 +1200

    state rendered as list using .map() method and passing in arrow function to return JSX for each item in state array

--- src/App.js
+++ src/App.js
@@ -51,21 +51,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}> child element of Person 1
-          </Person>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Johnny!')} 
-            changed={this.nameChangedHandler}
-            > child element of Person 2          
-          </Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}>
-          </Person>
+          {this.state.persons.map(person => {
+              return <Person
+              name={person.name}
+              age={person.age} />
+            }
+          )}
         </div>
       );
     }
